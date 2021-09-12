5143ec161eccca4b0434c110edcd338b7ab30b67
Shaad Quazi <shaadquazi@gmail.com>
2021-09-12 16:02:05 -0400

    Added heuristics.py - includes Euclidean, Manhattan, and Chebyshev distance

---
+++ heuristics.py
@@ -0,0 +1,82 @@
+def __valid_input(A: list, B: list) -> bool:
+    """
+    Raises an exception if the input is not valid.
+    Valid parameter should be a list with length 2.
+
+    Parameters:
+    ----------
+    A : 2D coordinates of A as a list.
+    B : 2D coordinates of B as a list.
+
+    Returns:
+    -------
+    valid_input : True
+    """
+    if not isinstance(A, list) or not isinstance(B, list):
+        raise TypeError("Argument expected to be a list")
+    if not len(A) == 2 or not len(B) == 2:
+        raise ValueError("Argument length should be 2")
+    return True
+
+def euclidean(A: list, B: list) -> float:
+    """
+    Computes the Euclidean distance between two coordinate.
+    The Euclidean distance between two coordinate A and B, is defined as:
+    d((Ax, Ay), (Bx, By)) = ((Ax - Bx) ** 2 + (Ay - By) ** 2) ** 0.5
+
+    Parameters:
+    ----------
+    A : 2D coordinates of A as a list.
+    B : 2D coordinates of B as a list.
+
+    Returns:
+    -------
+    euclidean : float
+        The Euclidean distance between coordinate A and B.
+    """
+    if __valid_input(A, B):
+        Ax, Ay = A
+        Bx, By = B
+        return ((Ax - Bx) ** 2 + (Ay - By) ** 2) ** 0.5
+
+def manhattan(A: list, B: list) -> float:
+    """
+    Computes the Manhattan distance between two coordinate.
+    The Manhattan distance between two coordinate A and B, is defined as:
+    d((Ax, Ay), (Bx, By)) = |Ax - Bx| + |Ay - By|
+
+    Parameters:
+    ----------
+    A : 2D coordinates of A as a list.
+    B : 2D coordinates of B as a list.
+
+    Returns:
+    -------
+    manhattan : float
+        The Manhattan distance between coordinate A and B.
+    """
+    if __valid_input(A, B):
+        Ax, Ay = A
+        Bx, By = B
+        return (abs(Ax - Bx) + abs(Ay - By))
+
+def chebyshev(A: list, B: list) -> float:
+    """
+    Computes the Chebyshev distance between two coordinate.
+    The Chebyshev distance between two coordinate A and B, is defined as:
+    d((Ax, Ay), (Bx, By)) = max(|Ax - Bx|, |Ay - By|)
+
+    Parameters:
+    ----------
+    A : 2D coordinates of A as a list.
+    B : 2D coordinates of B as a list.
+
+    Returns:
+    -------
+    chebyshev : float
+        The Chebyshev distance between coordinate A and B.
+    """
+    if __valid_input(A, B):
+        Ax, Ay = A
+        Bx, By = B
+        return (max(abs(Ax - Bx), abs(Ay - By)))
