c5a69af042a3897c4d533b561820a14fa5203624
Shaad Quazi <shaadquazi@gmail.com>
2021-09-26 10:51:10 -0400

    Added comments and option arg

--- func_Astar.py
+++ func_Astar.py
@@ -2,7 +2,7 @@ from PriorityQueue import PriorityQueue, PrioritizedItem
 from gridworld import Cell, Gridworld
 
 
-def func_Astar(start: Cell, goal: list, maze: Gridworld, dim: int, option: int) -> Cell:
+def func_Astar(start: Cell, goal: list, maze: Gridworld, dim: int, option: int = 0) -> Cell:
     """
     Create a cell.
     Parameters:
@@ -14,20 +14,24 @@ def func_Astar(start: Cell, goal: list, maze: Gridworld, dim: int, option: int) 
 
     Returns:
     -------
-    cell, status_string: Returns cell if goal node is found along with a status string.
+    cell, status_string, List[visited, trajectory]: Returns cell if goal node is found along with a status string.
     """
     fringe = PriorityQueue()
     fringe.insert(PrioritizedItem(start.get_fscore(), start))
 
     visited = set()
-    trajectory = [start.get_index()]
+    trajectory = []
 
-    while not fringe.isEmpty():
+    while not fringe.is_empty():
         current_item = fringe.delete()
         current = current_item.item
-        trajectory.append(current.get_index())
+
         if current.get_index() in visited:
             continue
+
+        # We will adding to trajectory as we have taken a step forward
+        trajectory.append(current.get_index())
+        # Adding only not visited cells
         visited.add(current.get_index())
 
         if [current.x, current.y] == goal:
@@ -36,8 +40,11 @@ def func_Astar(start: Cell, goal: list, maze: Gridworld, dim: int, option: int) 
         currentg = current.get_gscore()
         children = current.get_children()
 
+        # Adding our field of view to our PriorityQueue
         for child in children:
             maze_child = maze.get_cell(child[0], child[1])
+
+            # If child is not a blocked cell or already visited
             if maze_child.get_flag() != 1 and (child[0] * dim + child[1] not in visited):
                 c = Cell(child[0], child[1], (currentg + 1),
                          dim, parent=current, option=option)
